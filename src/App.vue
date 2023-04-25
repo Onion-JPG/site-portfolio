7d3e5ef8915a939f6933c0abe525832146d1bce0
Onion-JPG <caden8898@gmail.com>
2023-04-24 23:18:48 -0400

    added first text box and some starting text.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
-
+    import about from "./components/about.vue";
 </script>
 
 <template>
+    <about></about>
   
 </template>
 
-<style scoped>
+<script>
+    export default {
+        name: "App",
+        data() {
+            return {
+                displayIntro: true,
+            };
+        },
+        components: {
+            about, 
+        },
+    };
+</script>
 
-</style>
---
+++ src/components/about.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+    import colorText from "./colorText.vue";
+
+    export default {
+    name: "about",
+    data() {
+        return {
+            aboutLine1: true,
+            aboutLine2: false,
+            displayAbout: true,
+        };
+    },
+    components: {
+        colorText,
+    },
+    props:{
+        displayProject: Boolean
+    }
+    };
+</script>
+
+<template>
+    <div class="container rounded-lg w-[650px] m-2 relative mb-24" style="background-color: #2A2A2A;">
+        <div class="flex flex-row">
+            <div class="container flex justify-start text-white p-2 pl-4 italic">
+                <h2>About Me</h2>
+            </div>
+            <div class="container flex justify-end text-white p-2 gap-2 max-w-[800px]">
+                <button class="rounded-full w-5 h-5 bg-[#00CA4E] hover:bg-[#33D571]"></button>
+                <button class="rounded-full w-5 h-5 bg-[#FFBD44] hover:bg-[#FFCA69]"></button>
+                <button class="rounded-full w-5 h-5 bg-[#FF605C] hover:bg-[#FF807D]"></button>
+             </div>
+        </div>
+    
+        <div class="container bg-slate-400 min-h-[260px] rounded-bl-lg rounded-br-lg text-white p-2 font-mono" style="background-color: #191919;">
+            <div class="text-[#FFB0AE] italic pl-3 flex flex-wrap">
+                <colorText
+                    :printSpeed="20"
+                    :printStatus="aboutLine1"
+                    @printDone="aboutLine2 = true"
+                    printableText="// Hi my name is Caden Citro A.K.A Onion_jpg"
+                ></colorText>
+            </div>
+            <div class="text-[#FFB0AE] italic pl-3 flex flex-wrap">
+                <colorText
+                    :printSpeed="20"
+                    :printStatus="aboutLine2"
+                    
+                    printableText="// I am a student studying Computer Science and Games at Miami University"
+                ></colorText>
+            </div>
+        </div>   
+    </div>                           
+</template>
