bd7d882959a40d7dd4dc8351d482f3a54ecb215f
Onion-JPG <caden8898@gmail.com>
2023-04-25 10:22:06 -0400

    made terminal look more like a MacOS terminal

--- src/components/about.vue
+++ src/components/about.vue
@@ -22,14 +22,14 @@
 <template>
     <div class="container rounded-lg w-[700px] m-2 relative mb-24" style="background-color: #2A2A2A;">
         <div class="flex flex-row">
-            <div class="container flex justify-start text-white p-2 pl-4 italic">
-                <h2>About Me</h2>
-            </div>
-            <div class="container flex justify-end text-white p-2 gap-2 max-w-[800px]">
+            <div class="container flex justify-start text-white p-2 gap-2" style="width : 75%">
                 <button class="rounded-full w-5 h-5 bg-[#00CA4E] hover:bg-[#33D571]"></button>
                 <button class="rounded-full w-5 h-5 bg-[#FFBD44] hover:bg-[#FFCA69]"></button>
                 <button class="rounded-full w-5 h-5 bg-[#FF605C] hover:bg-[#FF807D]"></button>
              </div>
+            <div class="container flex text-white p-2 pl-4 italic">
+                <h2>About Me</h2>
+            </div>
         </div>
     
         <div class="container bg-slate-400 min-h-[260px] rounded-bl-lg rounded-br-lg text-white p-2 font-mono" style="background-color: #191919;">
