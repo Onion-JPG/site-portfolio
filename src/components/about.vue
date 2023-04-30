4480a6b17366049db6c87acbb823432c5fc6e825
Onion-JPG <caden8898@gmail.com>
2023-04-30 16:21:47 -0400

    added a link

--- src/components/about.vue
+++ src/components/about.vue
@@ -7,6 +7,10 @@
         return {
             aboutLine1: true,
             aboutLine2: false,
+            aboutLine3: false,
+            aboutLine4: false,
+            aboutLine5: false,
+            aboutLine6: false,
             displayAbout: true,
         };
     },
@@ -41,12 +45,43 @@
                     printableText="// Hi my name is Caden Citro A.K.A Onion_jpg"
                 ></colorText>
             </div>
+
             <div class="text-[#FFB0AE] italic pl-3 flex flex-wrap">
                 <colorText
                     :printSpeed="20"
                     :printStatus="aboutLine2"
-                    
+                    @printDone="aboutLine3 = true"
                     printableText="// I am a student studying Computer Science and Games at Miami University"
+                ></colorText>
+            </div>
+
+            <div class="text-[#69C06C] italic pl-3 flex flex-wrap">
+                <colorText
+                    :printSpeed="20"
+                    :printStatus="aboutLine3"
+                    @printDone="aboutLine4 = true"
+                    printableText="links = {"
+                ></colorText>
+            </div>
+           
+            <div class="text-[#69C06C] flex flex-row" style="padding-left:1.5rem">
+                <colorText
+                    :printSpeed="50"
+                    :printStatus="aboutLine4"
+                    @printDone="aboutLine5 = true"
+                    printableText="Github:"
+                ></colorText>
+                <a v-if="aboutLine3" class="text-pink-400 underline" 
+                    href="https://github.com/Onion-JPG"
+                    target="_blank"
+                    >https://github.com/Onion-JPG</a>
+            </div>
+            
+            <div class="text-[#69C06C] italic pl-3 flex flex-wrap">
+                <colorText
+                    :printSpeed="20"
+                    :printStatus="aboutLine5"
+                    printableText="}"
                 ></colorText>
             </div>
         </div>   
